d23dd62199db10cadbff94e38e3afd04584b6eeb
Aarushi <aarushimandloi744@gmail.com>
2025-03-25 16:01:12 +0530

    sunrecursive

---
+++ recursive_sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fact(int);
+int main(){
+ int n;
+ scanf("%d",&n);
+ 
+ int res = fact(n);
+ 
+ printf("%d",res);
+    return 0;
+} 
+
+int fact(int n){
+for(int i=1;i<=n;i++){ 
+    if(n==1 || n<0) return 1;
+    else return n+fact(n-1);
+}     
+}
